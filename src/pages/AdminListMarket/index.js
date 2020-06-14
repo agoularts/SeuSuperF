82c1736dc42f93baa4b2051fc1d4218d8a7efc3a
Aline Goulart <alinegs.91@live.com>
2020-06-14 02:15:30 -0300

    admin pages

--- src/pages/AdminListMarket/index.js
+++ src/pages/AdminListMarket/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { FiEdit } from 'react-icons/fi';
+import { FiEdit, FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api';
 import { validaToken } from '../../services/auth';
