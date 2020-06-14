3fa07a7652dbf33e1f4ed1936fc4765879586709
Aline Goulart <alinegs.91@live.com>
2020-06-14 01:58:07 -0300

    routes  config

--- src/pages/AdminListMarket/index.js
+++ src/pages/AdminListMarket/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiEdit } from 'react-icons/fi';
 
 import api from '../../services/api';
